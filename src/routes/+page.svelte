370dd4423460e2dcc1c312f9361b7bdbb3377288
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-02-16 13:22:39 +1300

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene from '$lib/components/Scene.svelte';
+	import '../app.css';
+</script>
+
+<div>
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
+
+<style>
+	div {
+		width: 100vw;
+		height: 100vh;
+		background: rgb(13, 19, 32);
+		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+	}
+</style>
